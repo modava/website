43302072f2d0fdae9490b9b4bcba5f23ed68ee51
Mong Dao <mongdaovan86@yahoo.com>
2020-08-08 16:04:21 +0700

    update 08082020

--- src/widgets/NavbarWidgets.php
+++ src/widgets/NavbarWidgets.php
@@ -10,6 +10,9 @@ class NavbarWidgets extends \yii\base\Widget
 
     public function run()
     {
-        return $this->render('navbarWidgets', []);
+        if(CONSOLE_HOST == 1)
+            return $this->render('navbarWidgets', []);
+        else
+            return '';
     }
 }
