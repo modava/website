9604644c5e0dd990db0237f9b4563e7ecb06d1ed
Mong Dao <mongdaovan86@yahoo.com>
2020-08-12 19:27:04 +0700

    delete partner

--- src/migration/m200808_073047_create_talbe_website_seo.php
+++ src/migration/m200808_073047_create_talbe_website_seo.php
@@ -12,7 +12,26 @@ class m200808_073047_create_talbe_website_seo extends Migration
      */
     public function safeUp()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
+        }
 
+        $this->createTable('{{%website_partner}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(255)->notNull(),
+            'image' => $this->string(255)->null(),
+            'link' => $this->string(255)->null(),
+            'status' => $this->smallInteger(1)->notNull()->defaultValue(1),
+            'language' => $this->string(25)->null(),
+            'created_at' => $this->integer(11)->null(),
+            'updated_at' => $this->integer(11)->null(),
+            'created_by' => $this->integer(11)->null(),
+            'updated_by' => $this->integer(11)->null(),
+        ], $tableOptions);
+
+        $this->addForeignKey('fk-website_partner-created_by-user-id', 'website_partner', 'created_by', 'user', 'id', 'RESTRICT', 'CASCADE');
+        $this->addForeignKey('fk-website_partner-updated_by-user-id', 'website_partner', 'updated_by', 'user', 'id', 'RESTRICT', 'CASCADE');
     }
 
     /**
@@ -20,23 +39,6 @@ class m200808_073047_create_talbe_website_seo extends Migration
      */
     public function safeDown()
     {
-        echo "m200808_073047_create_talbe_website_seo cannot be reverted.\n";
-
-        return false;
+        $this->dropTable('{{%website_partner}}');
     }
-
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
-
-    }
-
-    public function down()
-    {
-        echo "m200808_073047_create_talbe_website_seo cannot be reverted.\n";
-
-        return false;
-    }
-    */
 }
