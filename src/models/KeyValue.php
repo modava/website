c7e81d97026545845d9d726965bfbdff31ade57e
Mong Dao <mongdaovan86@yahoo.com>
2020-09-22 16:22:23 +0700

    update 22092020

--- src/models/KeyValue.php
+++ src/models/KeyValue.php
@@ -38,7 +38,6 @@ class KeyValue extends KeyValueTable
                 ],
                 'timestamp' => [
                     'class' => 'yii\behaviors\TimestampBehavior',
-                    'preserveNonEmptyValues' => true,
                     'attributes' => [
                         ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
                         ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
